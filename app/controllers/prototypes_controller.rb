272fbb360208fab2d6a9dbe7dc0ab56178308b29
ukkiy-ray <showy13f207ag@gmail.com>
2020-10-18 21:35:51 +0900

    prototypes_controllerのmove_to_indexアクションno

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, only: [:edit]
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
@@ -50,7 +50,8 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
       redirect_to action: :index
     end
   end
